f7ee5142b805db25c2b5912d09868a92bd8dbafe
Molnar Eduard <edimolnar@posteo.ro>
2025-04-01 22:47:10 +0300

    Start work on StoryChanger script and Resource loading

---
+++ Assets/Scripts/StoryChanger.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+using TMPro;
+
+public class StoryChanger : MonoBehaviour
+{
+	public TextMeshProUGUI storyText00, storyText01, buttonText1, buttonText2;
+
+	public void setText()
+	{
+		storyText00.text = "Asdsd";
+	}
+}
+
